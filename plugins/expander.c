334926cc3089ed56cf4bd2ae79fb2085249c8bf8
Eylon Iyov <eylon.iyov@post.runi.ac.il>
2025-08-12 14:50:19 +0000

    added expander

--- plugins/expander.c
+++ plugins/expander.c
@@ -3,6 +3,19 @@
 
 const char *plugin_transform(const char *input)
 {
+    int length = strlen(input);
+    char *output = malloc((length * 2 + 1) * sizeof(char));
+
+    if (!output)
+        return NULL;
+
+    for (int i = 0, j = 0; i < length; i++)
+    {
+        output[j++] = input[i];
+        output[j++] = ' ';
+    }
+    output[length * 2] = '\0';
+    return output;
 }
 
 __attribute__((visibility("default")))
