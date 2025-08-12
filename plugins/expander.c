acf090710aa8f0329a958ed50e11affd0787dbcc
Eylon Iyov <eylon.iyov@post.runi.ac.il>
2025-08-12 14:39:31 +0000

    added flipper

---
+++ plugins/expander.c
@@ -0,0 +1,13 @@
+#include "plugin_common.h"
+#include <stdio.h>
+
+const char *plugin_transform(const char *input)
+{
+}
+
+__attribute__((visibility("default")))
+const char *
+plugin_init(int queue_size)
+{
+    return common_plugin_init((void *)plugin_transform, "expander", queue_size);
+}
