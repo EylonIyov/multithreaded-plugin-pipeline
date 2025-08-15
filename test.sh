4ea9ba10fd06748fcb5e49fcdc25ef5552fce9d5
Eylon Iyov <eylon.iyov@post.runi.ac.il>
2025-08-15 09:30:04 +0000

    added executing and writing premissions to all files

--- test.sh
+++ test.sh
@@ -19,3 +19,16 @@ print_error()
 { 
     echo -e "${RED}[ERROR]${NC} $1" 
 }
+
+for plugin_name in logger uppercaser rotator flipper expander typewriter; do 
+    print_status "Building plugin: $plugin_name" 
+    gcc -fPIC -shared -o output/${plugin_name}.so \ 
+        plugins/${plugin_name}.c \ 
+        plugins/plugin_common.c \ 
+        plugins/sync/monitor.c \ 
+        plugins/sync/consumer_producer.c \ 
+        -ldl -lpthread || { 
+        print_error "Failed to build $plugin_name" 
+        exit 1 
+        }    
+done 
