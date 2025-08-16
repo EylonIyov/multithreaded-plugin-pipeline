97ed4c9673e8a1895b49532c5c2dd5c4d8dc28c6
Eylon Iyov <eylon.iyov@post.runi.ac.il>
2025-08-16 12:54:40 +0000

    added init to plugins v2

--- plugins/logger.c
+++ plugins/logger.c
@@ -11,3 +11,9 @@ const char *plugin_transform(const char *input)
 
     return output;
 }
+__attribute__((visibility("default")))
+const char *
+plugin_init(int queue_size)
+{
+    return common_plugin_init(plugin_transform, "logger", queue_size);
+}
